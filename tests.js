f587cba43c26f74cb09d7d62723a3e75b609b865
TY <mr.tyeung@gmail.com>
2022-07-07 14:45:01 -0700

    enhancement: added error handling and tabbing to the messages

--- tests.js
+++ tests.js
@@ -2,31 +2,34 @@ const AI = require("./battleship");
 
 const ai = new AI();
 
-// const EQUALITY_OPS = {
-//   toBeTrue: (lhs) => (rhs) => rhs === true,
-//   toEqual: (lhs, rhs) => lhs === rhs
-// };
-
 function describe(msg, cb) {
-  console.log(msg);
+  console.log(`${msg}`);
   cb();
 }
 
 function test(msg, cb) {
-  console.log(msg);
+  console.log(`\t${msg}`);
   cb();
 };
 
-function expect(lhs) {
+function expect(msg, lhs) {
   return {
-    toEqual: (rhs) => lhs === rhs
+    toEqual: (rhs) => {
+      if (lhs === rhs) {
+        console.log(`\t\texpect ${msg} '${lhs}' to equal '${rhs}': ${lhs === rhs}`)
+      } else {
+        const errMsg = `${msg} '${lhs}' IS NOT EQUAL to ${rhs}`;
+        throw new Error(errMsg)
+      }
+    }
   }
 };
 
 describe('Battleship test suite:', () => {
-  test('  board dimensions ', () => {
+  test('board dimensions ', () => {
     const [ width, height ] = ai.getBoardDimensions();
-    console.log(expect(width).toEqual(10));
+    expect('default board width', width).toEqual(10);
+    expect('default board height', height).toEqual(10);
 
   })
 });
